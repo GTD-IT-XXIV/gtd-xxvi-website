7dc254555f10d54b48c4a485452b84a9159d8be1
Kristian Hadinata Achwan <kristianachwan@gmail.com>
2023-12-25 20:22:07 +0100

    refactor: standardize method name

--- src/server/controllers/tickets-router.ts
+++ src/server/controllers/tickets-router.ts
@@ -11,13 +11,11 @@ export const ticketsRouter = createTRPCRouter({
     .query(({ ctx, input }) => {
       return ctx.prisma.ticket.findUnique({ where: { id: input } });
     }),
-  getManyByEventId: publicProcedure
-    .input(z.number())
-    .query(({ ctx, input }) => {
-      return ctx.prisma.ticket.findMany({
-        where: { timeslot: { eventId: input } },
-      });
-    }),
+  getManyByEvent: publicProcedure.input(z.number()).query(({ ctx, input }) => {
+    return ctx.prisma.ticket.findMany({
+      where: { timeslot: { eventId: input } },
+    });
+  }),
   deleteById: publicProcedure.input(z.number()).query(({ ctx, input }) => {
     return ctx.prisma.ticket.delete({
       where: { id: input },
