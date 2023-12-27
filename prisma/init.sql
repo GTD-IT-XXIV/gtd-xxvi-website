cf5a1a26d635d08329eed6d1ae36e8b5f649c2ad
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-12-27 23:15:23 +0800

    feat: add init db script

---
+++ prisma/init.sql
@@ -0,0 +1,197 @@
+-- Insert Sample Data
+-- Insert Sample Events
+INSERT INTO
+  "Event" (name, description, "startDate", "endDate")
+VALUES
+  (
+    'Sample Event 1',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2023-11-07',
+    '2023-11-08'
+  ),
+  (
+    'Sample Event 2',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2023-03-12',
+    '2023-03-14'
+  ),
+  (
+    'Sample Event 3',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-05-10',
+    '2024-05-11'
+  ),
+  (
+    'Sample Event 4',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-08-21',
+    '2024-08-21'
+  ),
+  (
+    'Sample Event 5',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-02-21',
+    '2024-02-22'
+  ),
+  (
+    'Sample Event 6',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-10-06',
+    '2024-10-08'
+  ),
+  (
+    'Sample Event 7',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-10-06',
+    '2024-10-08'
+  ),
+  (
+    'Sample Event 8',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-01-01',
+    '2024-01-03'
+  ),
+  (
+    'Sample Event 9',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-08-17',
+    '2024-08-17'
+  ),
+  (
+    'Sample Event 10',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vitae hendrerit mauris. Vestibulum diam purus, semper et enim dictum, pulvinar maximus turpis. Donec volutpat, enim quis viverra suscipit, felis nunc elementum metus, at condimentum velit massa finibus quam. Fusce nec nibh quis lectus imperdiet facilisis in id nisl. Duis sed mollis dui.',
+    '2024-09-15',
+    '2024-09-17'
+  );
+
+-- Insert Sample Bundles
+INSERT INTO
+  "Bundle" (
+    name,
+    details,
+    price,
+    quantity,
+    "remainingAmount",
+    "eventId"
+  )
+VALUES
+  (
+    'Sample Event 1 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    15.00,
+    1,
+    NULL,
+    1
+  ),
+  (
+    'Sample Event 2 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    5.00,
+    1,
+    NULL,
+    2
+  ),
+  (
+    'Sample Event 3 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    12.00,
+    1,
+    NULL,
+    3
+  ),
+  (
+    'Sample Event 4 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    6.00,
+    1,
+    NULL,
+    4
+  ),
+  (
+    'Sample Event 5 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    16.00,
+    1,
+    NULL,
+    5
+  ),
+  (
+    'Sample Event 6 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    8.00,
+    1,
+    NULL,
+    6
+  ),
+  (
+    'Sample Event 7 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    7.00,
+    1,
+    NULL,
+    7
+  ),
+  (
+    'Sample Event 8 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    14.00,
+    1,
+    NULL,
+    8
+  ),
+  (
+    'Sample Event 9 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    12.50,
+    1,
+    NULL,
+    9
+  ),
+  (
+    'Sample Event 10 Individual',
+    ARRAY[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Nunc pharetra fermentum lacus nec tempus. Morbi id tellus sollicitudin, semper massa a, semper erat.',
+      'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    ],
+    9.50,
+    1,
+    NULL,
+    10
+  );
