fe97f3f24938ebdce0779c2d7b7b494022b34a87
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-01-07 11:58:01 +0800

    refactor: move events query logic

--- src/app/events/gtdfest/page.tsx
+++ src/app/events/gtdfest/page.tsx
@@ -1,11 +1,53 @@
+"use client";
+
+import { useAtom } from "jotai";
 import Link from "next/link";
 
+import { eventDetailsAtom } from "@/lib/atoms/events-registration";
+import { api } from "@/trpc/provider";
+
 export default function GTDFestPage() {
+  const [eventDetails, setEventDetails] = useAtom(eventDetailsAtom);
+
+  const { data: gtdFest, isLoading: gtdfestIsLoading } =
+    api.events.getById.useQuery(9);
+  const { data: escapeRoom, isLoading: escapeRoomIsLoading } =
+    api.events.getById.useQuery(10);
+  const isLoading = gtdfestIsLoading || escapeRoomIsLoading;
+
+  function handleClickRegister() {
+    if (!gtdFest || !escapeRoom)
+      throw new Error("GTD Fest or Escape Room event not found");
+    setEventDetails({
+      ...eventDetails,
+      [gtdFest.id]: {
+        name: gtdFest.name,
+        quantity: 0,
+        bundle: null,
+        timeslot: null,
+      },
+      [escapeRoom.id]: {
+        name: escapeRoom.name,
+        quantity: 0,
+        bundle: null,
+        timeslot: null,
+      },
+    });
+    console.log("register button click");
+  }
+
+  // TODO: separate client and server logic to improve page performance
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <main>
       <h1 className="text-2xl font-semibold">GTD Fest x Escape Room Page</h1>
-      <Link href="/events/register/gtdfest">
-        <button type="button" className="p-2 bg-slate-200 hover:bg-slate-100">
+      <Link href="/events/register">
+        <button
+          type="button"
+          onClick={handleClickRegister}
+          className="p-2 bg-slate-200 hover:bg-slate-100"
+        >
           Register
         </button>
       </Link>
--- src/app/events/register/page.tsx
+++ src/app/events/register/page.tsx
@@ -1,5 +1,69 @@
-import { permanentRedirect } from "next/navigation";
+"use client";
+
+import { useAtom } from "jotai";
+import { useRouter } from "next/navigation";
+
+import BundlePopup from "@/components/bundle-popup";
+import BundlePopupContent from "@/components/bundle-popup-content";
+import RegistrationForm from "@/components/registration-form";
+import { useToast } from "@/components/ui/use-toast";
+
+import {
+  eventDetailsAtom,
+  eventsFormDataAtom,
+  registrationCompletionAtom,
+} from "@/lib/atoms/events-registration";
+import { api } from "@/trpc/provider";
 
 export default function RegistrationPage() {
-  return permanentRedirect("/events/register/gtdfest");
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const [formData] = useAtom(eventsFormDataAtom);
+  const [eventDetails] = useAtom(eventDetailsAtom);
+  const [registrationCompletion, setRegistrationCompletion] = useAtom(
+    registrationCompletionAtom,
+  );
+
+  const { data: booking, isLoading: bookingIsLoading } =
+    api.bookings.getByEmail.useQuery(formData.email);
+
+  function handleFormSubmit() {
+    const eventsBundleSelected = Object.keys(eventDetails).reduce(
+      (accum, eventId) => (accum &&= !!eventDetails[Number(eventId)]?.bundle),
+      true,
+    );
+
+    if (!eventsBundleSelected) {
+      toast({
+        variant: "destructive",
+        title: "Event bundle not selected",
+        description:
+          "Please select a bundle for all events before submitting the form.",
+      });
+      return;
+    }
+
+    setRegistrationCompletion({ ...registrationCompletion, register: true });
+    router.push("/events/book");
+  }
+
+  if (!bookingIsLoading && booking) router.push("/checkout");
+
+  return (
+    <>
+      <BundlePopup>
+        {Object.keys(eventDetails)
+          .map((key) => Number(key))
+          .map((eventId) => (
+            <BundlePopupContent
+              key={eventId}
+              eventId={eventId}
+              eventName={eventDetails[eventId]!.name}
+            />
+          ))}
+      </BundlePopup>
+      <RegistrationForm onSubmit={handleFormSubmit} />
+    </>
+  );
 }
