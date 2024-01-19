147f5ed54aba66f50ddcc701acbac33ad8b4f9e6
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-01-20 00:14:58 +0800

    feat: add searchparams prop to timeslots page

--- src/app/(registration)/register/timeslots/page.tsx
+++ src/app/(registration)/register/timeslots/page.tsx
@@ -1,15 +1,36 @@
 import { type Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Timeslots",
-};
+import { notFound } from "next/navigation";
+import { z } from "zod";
 
 /**
  * See {@link https://github.com/GTD-IT-XXIV/gtd-xxvi-website/issues/50 GitHub Issue}
  */
 import TimeSlotButton from "@/components/registration/timeslot-button";
 
-export default function TimeslotsPage() {
+export const metadata: Metadata = {
+  title: "Timeslots",
+};
+
+export default function TimeslotsPage({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
+  if (!searchParams.event) {
+    notFound();
+  }
+  const eventParams = z.coerce
+    .number()
+    .array()
+    .or(z.coerce.number())
+    .parse(searchParams.event);
+  let eventIds: number[] = [];
+  if (Array.isArray(eventParams)) {
+    eventIds = eventParams;
+  } else {
+    eventIds = [eventParams];
+  }
+
   const date = new Date();
   return (
     <section>
