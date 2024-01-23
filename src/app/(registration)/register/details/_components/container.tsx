54d70067e67d3da14d6f2074ef6a33f9a07b395e
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-01-24 01:58:33 +0800

    chore: remove unused component

--- src/app/(registration)/register/details/_components/container.tsx
+++ src/app/(registration)/register/details/_components/container.tsx
@@ -3,31 +3,13 @@
 import { useAtom } from "jotai";
 
 import BookingInfo from "./info";
-// import BookingParticipants from "./participants";
 import BookingReview from "./review";
 
 export default function BookingContainer() {
-  // const [bookingDetails, setBookingDetails] = useAtom(defaultBookingDetails);
-  //
-  // const handleBooking = (field: string, value: string) => {
-  //   setBookingDetails({ ...bookingDetails, [field]: value });
-  // };
-
-  // const handleParticipants = (field: string, value: string) => {
-  //   setParticipantDetails({ ...participantDetiails, [field]: value });
-  // };
-
-  // To test whether the output is correct
-  // const handleSubmit = () => {
-  //   console.log(bookingDetails);
-  //   console.log(participantDetiails);
-  // };
-
   return (
     <div>
       <BookingReview />
       <BookingInfo />
-      {/* <BookingParticipants handleParticipants={handleParticipants} /> */}
     </div>
   );
 }
