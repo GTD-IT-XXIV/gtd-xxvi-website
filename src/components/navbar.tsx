55beceb0283da3ac1d1e6eab272934144e4007ee
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-01-29 16:58:12 +0800

    feat: add navbar
    Refactor: move gtdfest page location

--- src/components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export type NavbarProps = {
-  className?: string;
-};
-
-export default function Navbar({ className }: NavbarProps) {
-  return <nav className={cn("", className)}>Navbar goes here</nav>;
-}
