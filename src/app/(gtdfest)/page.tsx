d9c599e31afe7f92d4b5dc8ef4f51338a0423e57
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-01-31 03:41:02 +0800

    feat: add desktop layout top section

--- src/app/(gtdfest)/page.tsx
+++ src/app/(gtdfest)/page.tsx
@@ -76,49 +76,58 @@ export default async function GTDFestPage() {
     <main className="bg-slate-900 text-white">
       {/* Top Section */}
       <section className="relative">
-        <Image src={gtdFestBg1} alt="Background 1" />
-        <div className="absolute inset-0 flex flex-col gap-4 items-center">
-          <hgroup className="w-full pt-8 text-center bg-[radial-gradient(circle_at_center_35%,_var(--tw-gradient-stops))] from-slate-900 from-25%">
+        <Image
+          src={gtdFestBg1}
+          alt="Background 1"
+          className="md:aspect-video object-cover"
+        />
+        <div className="absolute flex flex-col inset-0 bg-[radial-gradient(circle_at_center_35%,_var(--tw-gradient-stops))] md:bg-[radial-gradient(circle_at_25%_50%,_var(--tw-gradient-stops))] from-slate-900 from-25% md:from-10% md:-translate-x-10">
+          <div className="flex-1 flex flex-col md:flex-row gap-4 md:justify-center items-center">
             <Image
               src={logoGTDFest}
               alt="Logo GTD Fest"
-              className="h-[30rem] w-full object-cover"
+              className="h-1/2 md:h-auto w-auto md:w-1/2 aspect-[1/0.9] object-[50%_35%] object-cover"
             />
-            <h1 className="block text-xl font-serif text-shadow shadow-slate-900">
-              <div>{gtdFest.name}</div>
-              <div className="text-5xl text-gtd-primary-20 text-shadow-[0_0_4px_var(--tw-shadow-color)] shadow-gtd-primary-30">
-                Enchantium
+            <div className="flex flex-col items-center gap-4">
+              <hgroup className="text-center">
+                <h1 className="block text-xl md:text-2xl font-serif text-shadow shadow-slate-900">
+                  <div>{gtdFest.name}</div>
+                  <div className="text-5xl md:text-6xl text-gtd-primary-20 text-shadow-[0_0_4px_var(--tw-shadow-color)] shadow-gtd-primary-30">
+                    Enchantium
+                  </div>
+                  <p className="text-lg md:text-xl mt-2 opacity-65">X</p>
+                  <div>{escapeRoom.name}</div>
+                  <div className="text-5xl md:text-6xl text-red-600 text-shadow-[0px_0px_4px_var(--tw-shadow-color)] shadow-red-500">
+                    Nyctophobia
+                  </div>
+                </h1>
+                <p className="text-shadow md:text-2xl shadow-slate-900 mt-6">
+                  {startDateLabel} - {endDateLabel}
+                </p>
+              </hgroup>
+              <div className="flex md:flex-col justify-center gap-4 md:gap-3">
+                <Button
+                  type="button"
+                  className="bg-gtd-primary-30 hover:bg-gtd-primary-20 md:px-6 font-semibold"
+                  asChild
+                >
+                  <Link href="/register">Register</Link>
+                </Button>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="hover:bg-slate-200 md:px-6"
+                  asChild
+                >
+                  <Link href="#learn-more">Learn More</Link>
+                </Button>
               </div>
-              <p className="text-lg mt-2 opacity-65">X</p>
-              <div>{escapeRoom.name}</div>
-              <div className="text-5xl text-red-600 text-shadow-[0px_0px_4px_var(--tw-shadow-color)] shadow-red-500">
-                Nyctophobia
-              </div>
-            </h1>
-            <p className="text-shadow shadow-slate-900 mt-6">
-              {startDateLabel} - {endDateLabel}
-            </p>
-          </hgroup>
-          <div className="flex gap-4">
-            <Button
-              type="button"
-              className="bg-gtd-primary-30 hover:bg-gtd-primary-20 font-semibold"
-              asChild
-            >
-              <Link href="/register">Register</Link>
-            </Button>
-            <Button
-              type="button"
-              variant="secondary"
-              className="hover:bg-slate-200"
-              asChild
-            >
-              <Link href="#learn-more">Learn More</Link>
-            </Button>
+            </div>
           </div>
           {/* biar gk ketutupan navbar */}
-          <div id="learn-more" className="flex-1" />
-          <ChevronDown className="size-8 animate-pulse" />
+          <div id="learn-more" className="flex justify-center">
+            <ChevronDown className="size-8 animate-pulse" />
+          </div>
         </div>
       </section>
 
