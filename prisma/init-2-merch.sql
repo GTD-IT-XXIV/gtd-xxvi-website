b8f94ae42433b1e503a505c20d879c71dbc4858e
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-02-18 00:52:28 +0800

    chore: finalize merch stocks

--- prisma/init-2-merch.sql
+++ prisma/init-2-merch.sql
@@ -8,9 +8,9 @@ VALUES
     2,
     'Reversible Lanyard',
     ARRAY['Yellow/Black'],
-    100
+    71
   ),
-  (3, 'Playing Cards', ARRAY['Standard'], 40);
+  (3, 'Playing Cards', ARRAY['Standard'], 14);
 
 INSERT INTO
   "MerchBundle" (id, name, images, stock, price)
@@ -33,7 +33,7 @@ VALUES
       'https://utfs.io/f/8e95fe67-65f5-49cb-b6b2-88d0e22ef08b-v0k004.png',
       'https://utfs.io/f/8757c225-27eb-45a7-8911-b3e08cf8cc78-880brd.png'
     ],
-    999,
+    71,
     3.00
   ),
   (
@@ -42,7 +42,7 @@ VALUES
     ARRAY[
       'https://utfs.io/f/f3172f7a-6292-4dd4-8ff4-f2044eb86e76-fau8ff.png'
     ],
-    999,
+    14,
     8.00
   );
 
