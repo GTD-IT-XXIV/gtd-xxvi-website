d648c3f3a9ecd2e5374b8295517d4610ea00f935
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-02-22 17:33:04 +0800

    refactor: move routes

--- src/app/(gtdfest)/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function GTDFestError({
-  error,
-}: {
-  error: Error & { digest?: string };
-}) {
-  return <p className="text-white">Error! {error.message}</p>;
-}
--- src/app/(gtdfest)/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function GTDFestLoading() {
-  return <div className="bg-slate-900 size-full" />;
-}
