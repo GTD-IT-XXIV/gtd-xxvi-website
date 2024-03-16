48e9e205a570d081f40c906232cab6ea7876b938
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-03-16 10:14:33 +0800

    chore: add tanstack query devtools

--- src/app/_components/trpc-react-provider.tsx
+++ src/app/_components/trpc-react-provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/react-query";
 import { useState } from "react";
 
@@ -53,6 +54,7 @@ export default function TRPCReactProvider(props: {
       <api.Provider client={trpcClient} queryClient={queryClient}>
         {props.children}
       </api.Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
