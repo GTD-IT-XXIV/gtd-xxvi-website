15532c49380cef0e3226c6adf091bf3e000bc377
denzel-afk <denzeledanbanget@gmail.com>
2024-05-17 14:25:18 +0800

    Fixing some bugs

--- src/app/(content)/events/(events)/_components/TextParralaxContent.tsx
+++ src/app/(content)/events/(events)/_components/TextParralaxContent.tsx
@@ -1,54 +1,72 @@
-"use client"
-
-import React from "react";
+import React, { use } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
-import { type StaticImport } from "next/dist/shared/lib/get-img-props";
+import { Button } from "@/components/ui/button";
 
 export type TextParallaxContentProps = {
-  imgUrl: StaticImport;
+  imgUrl: string;
   heading: string;
   date: string;
   description: string;
   buttonLink?: string;
   buttonText?: string;
+  buttonDisabled?: boolean;
   children: React.ReactNode;
   alignLeft: boolean;
+  imageOpacity?: number;
 };
 
 const IMG_PADDING = 12;
 
-export default function TextParallaxContent({ imgUrl, heading, date, description, buttonLink, buttonText, children, alignLeft }: TextParallaxContentProps) {
+export default function TextParallaxContent({
+  imgUrl,
+  heading,
+  date,
+  description,
+  buttonLink,
+  buttonText,
+  buttonDisabled,
+  children,
+  alignLeft,
+  imageOpacity = 0.8,
+}: TextParallaxContentProps) {
   return (
-    <div>
-      <div className="relative h-[150vh]">
-        <Image
-          src={imgUrl}
-          alt="background-1"
-          style={{
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: `calc(100vh + ${IMG_PADDING * 0.5}px)`,
-            top: `-${IMG_PADDING * 0.5}px`,
-            opacity: 0.8,
-          }}
-          className="sticky z-0 overflow-hidden w-screen"
-        />
-        {/* Rendering StickyImage with provided imgUrl */}
-        <div className={`absolute inset-0 flex flex-col justify-center px-12 text-left md:text-${alignLeft ? 'left' : 'right'} ${alignLeft ? 'items-start' : 'items-end'}`}>
-        <div className={`w-full md:w-2/5 ${alignLeft ? '' : 'text-right'}`}>
-          <h1 className="text-4xl text-white font-semibold">{heading}</h1>
-          <h2 className="text-l text-white py-2 font-thin">{date}</h2>
-          <p className="text-l text-white font-thin pb-2">{description}</p>
-          {buttonLink && (
-            <Link href={buttonLink} passHref>
-              <button className="text-black bg-white px-4 py-2 rounded-md hover:bg-gray-400 hover:text-white transition duration-300 ease-in-out">
-                {buttonText}
-              </button>
-            </Link>
-          )}
+    <div style={{ position: "relative", height: "600px" }}>
+      <div className="relative h-full">
+        <div className="absolute inset-0" style={{ opacity: imageOpacity }}>
+          <Image
+            src={imgUrl}
+            alt="background-1"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            className="z-0"
+          />
         </div>
+        <div
+          className={`absolute bottom-16 flex flex-col justify-center px-12 text-left md:text-${
+            alignLeft ? "left" : "right"
+          } md:${alignLeft ? "items-start" : "items-end"}`}
+        >
+          <div className={`w-full md:w-1/2 md:${alignLeft ? "" : "text-right"}`}>
+            <h1 className="text-[1.875rem] px-6 text-white font-semibold md:text-[3rem] md:font-extrabold md:px-8">
+              {heading}
+            </h1>
+            <h2 className="text-[1rem] px-6 text-white py-2 font-normal md:font-semibold md:text-[1.5rem] md:px-8">
+              {date}
+            </h2>
+            <p className="text-[0.8rem] px-6 text-white font-extralight py-1 pb-2 md:font-normal md:text-[1rem] md:px-8">
+              {description}
+            </p>
+            {!buttonDisabled && buttonLink && (
+              <Link href={buttonLink} passHref>
+                <Button className="text-black mx-6 bg-white my-4 rounded-md hover:bg-gray-400 hover:text-white transition duration-300 ease-in-out md:mx-8 md:my-4 md:pl-4 md:py-4">
+                  {buttonText}
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
       <div className="container mx-auto">{children}</div>
--- src/app/(content)/events/(events)/page.tsx
+++ src/app/(content)/events/(events)/page.tsx
@@ -1,25 +1,16 @@
 import { type Metadata } from "next";
 import React from "react";
-
 import { api } from "@/server/trpc";
 
-import foto1 from "./_assets/rain-1.jpg";
-import foto2 from "./_assets/rain-2.jpg";
+import scbd from "./_assets/scbd.webp";
+import gtdfest from "../gtdfest/_assets/arcade-3.webp";
+import escaperoom from "../gtdfest/_assets/escape-room.webp";
 import TextParallaxContent from "./_components/TextParralaxContent";
+import dayjs from "dayjs";
 
 export const metadata: Metadata = {
   title: "Events",
 };
-
-// Helper function to format the date
-function formatDate(dateString: string) {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-}
 
 let isNextAlignLeft = true;
 
@@ -31,7 +22,7 @@ export default async function EventsPage() {
   return (
     <section>
       {/* Header */}
-      <div className="sticky top-0 z-10 flex items-center bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 py-2 shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+      <div className="sticky top-0 z-10 flex items-center bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 py-2 shadow-lg transition duration-300 ease-in-out transform hover:scale-[1.03] md:top-16">
         <h1 className="text-white text-lg pl-8 font-medium">Latest Events</h1>
         <hr className="flex-grow bg-white mx-4 h-0.4"></hr>
       </div>
@@ -39,16 +30,34 @@ export default async function EventsPage() {
       {events.map((event) => {
         const alignLeft = isNextAlignLeft;
         isNextAlignLeft = !isNextAlignLeft;
+        const startDateLabel = dayjs.utc(event.startDate).format("D MMM YYYY");
+        const endDateLabel = dayjs.utc(event.endDate).format("D MMM YYYY");
+
+        // Determine the image to use based on the event name
+        let imgUrl = scbd;
+        if (event.name === "GTD Fest") {
+          imgUrl = gtdfest;
+        } else if (event.name === "Escape Room") {
+          imgUrl = escaperoom;
+        }else if(event.name === "Subcommittee Bonding Day"){
+          imgUrl = scbd;
+        }
+
+        let buttonDisabled = false;
+        if(event.name === "Subcommittee Bonding Day"){
+          buttonDisabled = true;
+        }
 
         return (
           <TextParallaxContent
             key={event.name}
-            imgUrl={foto2}
+            imgUrl={imgUrl.src}
             heading={event.name}
-            date={`${formatDate(event.startDate.toString())} - ${formatDate(event.endDate.toString())}`}
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum ligula sit amet, posuere odio."
+            date={`${startDateLabel}`}
+            description={event.description}
             buttonLink={`/events/${event.name.split(" ").join("_")}`}
             buttonText="Learn More"
+            buttonDisabled={buttonDisabled}
             alignLeft={alignLeft}
           >
             <p></p>
@@ -57,17 +66,6 @@ export default async function EventsPage() {
         );
       })}
       {/* Example static content */}
-      <TextParallaxContent
-        // key={tidak ada}
-        imgUrl={foto1}
-        heading="SCBD 2023"
-        date="segitu - segitu"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum ligula sit amet, posuere odio."
-        alignLeft={true}
-      >
-        <p></p>
-        {/* Child content */}
-      </TextParallaxContent>
     </section>
   );
 }
