a49c2a58da8163818dce621d895c8f19f0b72bff
Klarissa Witania <yennytionardi15@gmail.com>
2024-06-07 08:50:04 +0800

    feat: add sponsors

--- src/app/(content)/(home)/_components/sponsors.tsx
+++ src/app/(content)/(home)/_components/sponsors.tsx
@@ -1,3 +1,73 @@
-export default function Sponsors() {
-  return <div>Sponsors</div>;
+"use client";
+
+import Image, { type StaticImageData } from "next/image";
+import Link from "next/link";
+import { useEffect, useRef } from "react";
+
+export type SponsorsProps = {
+  bgUrl: string;
+  logos: {
+    src: StaticImageData;
+    url: string;
+  }[];
+};
+
+export default function Sponsors({ bgUrl, logos }: SponsorsProps) {
+  const scrollerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scroller = scrollerRef.current;
+
+    function addAnimation() {
+      if (!scroller) {
+        return;
+      }
+
+      const innerScroller = scroller.querySelector(".scroll_inner");
+
+      if (!innerScroller) {
+        return;
+      }
+
+      const innerScrollerChildren = Array.from(innerScroller.children);
+
+      if (innerScroller.getAttribute("data-cloned") === "true") {
+        return;
+      }
+      innerScrollerChildren.forEach((item) => {
+        const extendedLogos = item.cloneNode(true) as HTMLElement;
+
+        innerScroller.appendChild(extendedLogos);
+      });
+
+      innerScroller.setAttribute("data-cloned", "true");
+    }
+
+    addAnimation();
+  }, []);
+  return (
+    <div
+      className="relative w-full h-[336px] content-center"
+      style={{
+        backgroundImage: `url(${bgUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="text-center text-white mb-6">
+        <h2 className="text-2xl font-serif">Sponsored by:</h2>
+      </div>
+      <div ref={scrollerRef} className="scroller">
+        <div className="scroll_inner flex flex-wrap gap-3 sm:gap-5 md:gap-10 animate-infinite_scroll">
+          {logos.map((logo, index) => {
+            return (
+              <Link key={index} href={logo.url} className=" p-1">
+                <Image src={logo.src} alt={`logo ${index + 1}`} height={100} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -129,10 +129,15 @@ export default {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        infinite_scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        infinite_scroll: "infinite_scroll 10s linear infinite",
       },
     },
   },
