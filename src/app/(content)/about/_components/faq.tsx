7bf281ec55d22c934dcde30f73b5b4428b7e7c32
Fay <faybeata.k@gmail.com>
2024-06-15 23:26:56 +0800

    feat: add faq accordion

--- src/app/(content)/about/_components/faq.tsx
+++ src/app/(content)/about/_components/faq.tsx
@@ -1,3 +1,40 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function FAQ() {
-  return <div>FAQ</div>;
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Question 1...</AccordionTrigger>
+        <AccordionContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum
+          fermentum magna, a malesuada est consectetur eget. Vivamus a
+          vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui,
+          id vulputate.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Question 2...</AccordionTrigger>
+        <AccordionContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum
+          fermentum magna, a malesuada est consectetur eget. Vivamus a
+          vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui,
+          id vulputate.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Question 3...</AccordionTrigger>
+        <AccordionContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum
+          fermentum magna, a malesuada est consectetur eget. Vivamus a
+          vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui,
+          id vulputate.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 }
