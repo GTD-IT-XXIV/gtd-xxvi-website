551aaba62262225eec796b5365c460e88ff57df4
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-06-17 01:52:23 +0800

    fix: fix lint issue

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,4 +1,4 @@
-import EmblaCarousel from "./_components/about-carousel";
+import AboutCarousel from "./_components/about-carousel";
 import Anthem from "./_components/anthem";
 import Contacts from "./_components/contacts";
 import FAQ from "./_components/faq";
@@ -6,8 +6,10 @@ import FAQ from "./_components/faq";
 export default function AboutPage() {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold text-center my-5 text-white">About PINTU GTD</h1>
-      <EmblaCarousel />
+      <h1 className="font-serif text-3xl text-center my-5 text-white">
+        About PINTU GTD
+      </h1>
+      <AboutCarousel />
       <FAQ />
       <Contacts />
       <Anthem />
