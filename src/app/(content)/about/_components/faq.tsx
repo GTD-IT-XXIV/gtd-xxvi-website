720c15366c1c3e652b9f40bea4377631ed23fad1
Fay <faybeata.k@gmail.com>
2024-06-23 00:42:40 +0800

    feat: integrated array to house faq content

--- src/app/(content)/about/_components/faq.tsx
+++ src/app/(content)/about/_components/faq.tsx
@@ -5,36 +5,19 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import { faqdatas } from "./faq_data.js";
+
 export default function FAQ() {
+  const faqarray = faqdatas?.map((faqdata) => (
+    <AccordionItem key={faqdata.index} value={faqdata.index}>
+      <AccordionTrigger>{faqdata.question}</AccordionTrigger>
+      <AccordionContent>{faqdata.answer}</AccordionContent>
+    </AccordionItem>
+  ));
+
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Question 1...</AccordionTrigger>
-        <AccordionContent>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum
-          fermentum magna, a malesuada est consectetur eget. Vivamus a
-          vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui,
-          id vulputate.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Question 2...</AccordionTrigger>
-        <AccordionContent>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum
-          fermentum magna, a malesuada est consectetur eget. Vivamus a
-          vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui,
-          id vulputate.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Question 3...</AccordionTrigger>
-        <AccordionContent>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum
-          fermentum magna, a malesuada est consectetur eget. Vivamus a
-          vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui,
-          id vulputate.
-        </AccordionContent>
-      </AccordionItem>
+    <Accordion type="single" collapsible className="w-full font-serif">
+      {faqarray}
     </Accordion>
   );
 }
