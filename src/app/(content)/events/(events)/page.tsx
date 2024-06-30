fa0289bbdf061cc0aa924818dda62706724b0024
Klarissa Witania <yennytionardi15@gmail.com>
2024-06-30 16:59:53 +0800

    feat: add events page

--- src/app/(content)/events/(events)/page.tsx
+++ src/app/(content)/events/(events)/page.tsx
@@ -1,14 +1,18 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import { type Metadata } from "next";
+import Image from "next/image";
 import React from "react";
 
 import { api } from "@/server/trpc";
 
+import TopSection from "../../(home)/_components/top-section";
 import gtdfest from "../gtdfest/_assets/arcade-3.webp";
 import escaperoom from "../gtdfest/_assets/escape-room.webp";
+import eventsPageMap from "./_assets/events-page-map.webp";
 import scbd from "./_assets/scbd.webp";
 import TextParallaxContent from "./_components/TextParralaxContent";
+import ViewDetailText from "./_components/view-detail-text";
 
 dayjs.extend(utc);
 
@@ -22,6 +26,67 @@ export default async function EventsPage() {
 
   return (
     <section>
+      {/* Events page */}
+      <TopSection />
+      <div className="relative">
+        <Image
+          src={eventsPageMap}
+          alt="Event Page Map"
+          unoptimized
+          className="w-full"
+        />
+        <div className="absolute inset-12 flex flex-col items-center mt-44 space-y-12">
+          <ViewDetailText
+            day="DAY 1"
+            title="NIGHT GAME"
+            place=""
+            date=""
+            time=""
+          />
+          <div>
+            <div className="bg-white bg-auto h-72 w-96 bg-opacity-40">
+              Pic 1
+            </div>
+          </div>
+          <ViewDetailText
+            day="DAY 2"
+            title="BEACH DAY"
+            place=""
+            date=""
+            time=""
+          />
+          <div>
+            <div className="bg-white bg-auto h-72 w-96 bg-opacity-40">
+              Pic 2
+            </div>
+          </div>
+          <ViewDetailText
+            day="DAY 3"
+            title="AROUND SG"
+            place=""
+            date=""
+            time=""
+          />
+          <div>
+            <div className="bg-white bg-auto h-72 w-96 bg-opacity-40">
+              Pic 3
+            </div>
+          </div>
+          <ViewDetailText
+            day="DAY 4"
+            title="AWARDS NIGHT"
+            place=""
+            date=""
+            time=""
+          />
+          <div>
+            <div className="bg-white bg-auto h-72 w-96 bg-opacity-40">
+              Pic 4
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Header */}
       <div className="sticky top-0 z-10 flex items-center bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 py-2 shadow-lg transition duration-300 ease-in-out transform hover:scale-[1.03] md:top-16">
         <h1 className="text-white text-lg pl-8 font-medium">Latest Events</h1>
