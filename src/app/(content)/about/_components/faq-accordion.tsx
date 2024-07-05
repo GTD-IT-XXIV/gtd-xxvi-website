1c5a657f936ebe417c0f6747199c7c08236e6c8f
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-07-05 21:23:25 +0700

    chore: update faq data

--- src/app/(content)/about/_components/faq-accordion.tsx
+++ src/app/(content)/about/_components/faq-accordion.tsx
@@ -12,21 +12,27 @@ import { cn } from "@/lib/utils";
 export const faqdatas = [
   {
     index: "1",
-    question: "Question 1...",
+    question:
+      "What are the differences between GTD and school/hall orientation?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum fermentum magna, a malesuada est consectetur eget. Vivamus a vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui, id vulputate.",
+      "Our orientation is solely for Indonesian students who are studying in NTU, all programmes are welcome to join. Our aim is to foster freshies with Indonesian community in NTU.",
   },
   {
     index: "2",
-    question: "Question 2...",
+    question: "How can I to register?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum fermentum magna, a malesuada est consectetur eget. Vivamus a vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui, id vulputate.",
+      "You can register yourself by filling up the registration form attached in this page. If you have further questions feel free to DM us on instagram @pintugtd. To reach us via telegram, please refer to the Contact Us section on this page.",
   },
   {
     index: "3",
-    question: "Question 3...",
+    question: "Where, when, and how long will the programs be conducted?",
+    answer: "Please refer to the Events page on our website.",
+  },
+  {
+    index: "4",
+    question: "How much does the orientation cost?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum fermentum magna, a malesuada est consectetur eget. Vivamus a vestibulum nisi, in tincidunt nunc. Pellentesque cursus aliquet dui, id vulputate.",
+      "$25. This price includes our annual GTD T-Shirt, dinner & bus accommodation on August 3, and many more!",
   },
 ];
 
