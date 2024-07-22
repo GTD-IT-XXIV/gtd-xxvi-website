9b6291f2478b7b69ffa5a2e1704f73985c5964a2
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-07-22 22:55:01 +0800

    feat: add senior camp to past events

---
+++ prisma/init-4-seniorcamp.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+  "Event" (
+    name,
+    description,
+    location,
+    "startDate",
+    "endDate"
+  )
+VALUES
+  (
+    'Senior Camp',
+    'What better way to spend the last few days of summer break by reminiscing GTD experience with your friends!!',
+    'NTU, Siloso beach, & Singapore Central Area',
+    '2024-07-19 19:15:00',
+    '2024-07-21 18:30:00'
+  );
